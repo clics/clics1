609db67eaae10ba54c6d8fa8daef65149344ea3e
LinguList <mattis.list@posteo.de>
2014-02-07 15:07:54 +0100

    added script for subgraph-extraction

---
+++ get_cluster.py
@@ -0,0 +1,48 @@
+# author   : Johann-Mattis List
+# email    : mattis.list@uni-marburg.de
+# created  : 2014-02-07 14:37
+# modified : 2014-02-07 14:37
+"""
+Load the networkx-graph and retrieve a specific cluster.
+"""
+
+__author__="Johann-Mattis List"
+__date__="2014-02-07"
+
+import networkx as nx
+import igraph as ig
+from sys import argv
+
+gml = ig.read('output/clics.gml')
+
+g = nx.Graph()
+
+for edge in gml.es:
+    source = gml.vs[edge.source]['concept']
+    target = gml.vs[edge.target]['concept']
+    weight = edge['weight']
+    g.add_edge(source,target,weight=weight)
+
+subg = nx.Graph()
+queue = [(argv[1], g.edge[argv[1]],0)]
+
+while queue:
+    
+    source,neighbors,generation = queue.pop(0)
+
+    for n,d in neighbors.items():
+        if d['weight'] > 3:
+            subg.add_edge(source,n,**d)
+            queue += [(n,g.edge[n],generation+1)]
+        elif d['weight'] > 2:
+            subg.add_edge(source,n,**d)
+
+    if generation > 2:
+        break
+
+nx.write_gml(subg, argv[1]+str(len(subg.nodes()))+'.gml')
+
+from clics_lib.gml2json import *
+gml2json(argv[1]+str(len(subg.nodes()))+'.gml')
+
+
